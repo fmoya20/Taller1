0b729fb200f323e359c5ab9f20c716f49f70dd57
fmoya <fmoya@Felipe>
2022-05-04 10:05:57 -0400

    version1

---
+++ src/main/java/test/App.java
@@ -0,0 +1,15 @@
+package test;
+
+import modelo.Ventana;
+
+/**
+ *
+ * @author bcast
+ */
+// Felipe Moya García - 20521590519
+public class App {
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        v.setVisible(true);
+    }
+}
